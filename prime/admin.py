6b4870195617bef5181d1482712d17483627efcb
Kartik Giri <kartikgiri92@gmail.com>
2020-01-25 19:21:29 +0530

    changes in prime_models

--- prime/admin.py
+++ prime/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+import prime.models as prime_models
 
 # Register your models here.
+admin.site.register(prime_models.Trend)
+admin.site.register(prime_models.Tweet)
+admin.site.register(prime_models.Log)
--- prime/models.py
+++ prime/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Trend(models.Model):
+    url = models.TextField()
+    name = models.TextField()
+    query = models.TextField()
+    positive_percentage = models.IntegerField(default = 0)
+    neutral_percentage = models.IntegerField(default = 0)
+    negative_percentage = models.IntegerField(default = 0)
+    total_tweet_volume = models.TextField(default = '0')
+    is_top_trending = models.BooleanField(default = False)
+    last_updated = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return(self.name)
+
+class Tweet(models.Model):
+    trend = models.ForeignKey(Trend, on_delete=models.CASCADE)
+    tweet_id = models.TextField()
+    text = models.TextField()
+    retweet_count = models.TextField()
+    favourite_count = models.TextField()
+    oembed_html = models.TextField()
+    compund_value = models.IntegerField(default = 0)
+    
+    def __str__(self):
+        return(self.text)
+
+class Log(models.Model):
+    last_updated = models.DateTimeField(auto_now = True)
+    message = models.TextField()
+    
+    def __str__(self):
+        return(self.message)
