07512f6fcbfbc0a42af7d2476c22add0d7eb01ea
Kartik Giri <kartikgiri92@gmail.com>
2020-01-28 02:04:18 +0530

    Bug FIxes

--- prime/urls.py
+++ prime/urls.py
@@ -4,12 +4,8 @@ from rest_framework import routers
 
 import prime.views as prime_views
 
-router = routers.DefaultRouter()
-# router.register(r'add-client', sud_views.AddClient)
-
 app_name = 'prime'
 urlpatterns = [
-    # url(r'^', include(router.urls)),
 
     path('foung/', prime_views.foung.as_view()),
 
@@ -17,35 +13,5 @@ urlpatterns = [
     path('get-trend/<int:id>/', prime_views.GetTrend.as_view()),
 
     path('get-active-trend/', prime_views.GetActiveTrend.as_view()),
-
-    # path('login/', pro_views.UserLogin.as_view()),
-    # path('logout/', pro_views.UserLogout.as_view()),
-    # path('create/', pro_views.CreateUser.as_view()),
-    # path('client/create/<int:id>/', sud_views.ClientDetail.as_view()),
-
-    # path('list-subjects/', sud_views.ListSubject.as_view()),
-    # path('create-subjects/', sud_views.SubjectDetail.as_view()),
-    # path('create-subjects/<int:id>/', sud_views.SubjectDetail.as_view()),
-
-    # path('list-standards/', sud_views.ListStandard.as_view()),
-    # path('create-standards/', sud_views.StandardDetail.as_view()),
-    # path('create-standards/<int:id>/', sud_views.StandardDetail.as_view()),
-
-    # path('list-substds/', sud_views.ListSubStd.as_view()),
-    # path('create-substds/', sud_views.SubStdDetail.as_view()),
-    # path('create-substds/<int:id>/', sud_views.SubStdDetail.as_view()),
-
-    # path('list-chapters/', sud_views.ListChapter.as_view()),
-    # path('create-chapters/', sud_views.ChapterDetail.as_view()),
-    # path('create-chapters/<int:id>/', sud_views.ChapterDetail.as_view()),
-
-    # path('list-topics/', sud_views.ListTopic.as_view()),
-    # path('create-topics/', sud_views.TopicDetail.as_view()),
-    # path('create-topics/<int:id>/', sud_views.TopicDetail.as_view()),
-
-    # path('list-questions/', sud_views.ListQuestion.as_view()),
-    # path('create-questions/', sud_views.QuestionDetail.as_view()),
-    # path('create-questions/<int:id>/', sud_views.QuestionDetail.as_view()),
-    # path('bulk-upload-questions/', sud_views.BulkUploadQuestion.as_view()),
     
 ]
