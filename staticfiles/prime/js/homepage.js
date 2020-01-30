ee1990a4bcbde4e1d503f94c1c77e729f489326e
Kartik Giri <kartikgiri92@gmail.com>
2020-01-31 05:27:39 +0530

    frontend js changes

--- staticfiles/prime/js/homepage.js
+++ staticfiles/prime/js/homepage.js
@@ -9,21 +9,78 @@ var fetch_trend_by_id_url = window_location_origin + "/api/prime/get-trend/";
 
 
 // Trend Info Section Variables
-var active_trend_row = document.querySelector("#active-trend-row")
-var active_trend_example_tag = document.querySelector("#active-trend-example-tag")
-var active_trend_not_available_tag = document.querySelector("#active-trend-not-available-tag")
+var trend_information_section = document.querySelector("#trend-information-section")
+var trend_information_not_available_tag = document.querySelector("#trend-information-not-available-tag")
 
 // Trend Info Section Functions
 
+function display_trend_information_data_unavailability_tag(){
+    trend_information_not_available_tag.style.display = "block";
+    trend_information_section.style.display = "none";
+}
+
+function hide_trend_information_data_unavailability_tag(){
+    trend_information_not_available_tag.style.display = "none";
+    trend_information_section.style.display = "block";
+}
+
+function fill_trend_information(json_obj_data){
+    let trend_information_elements = document.querySelectorAll(".trend-information")
+
+    // Trend Name
+    trend_information_elements[0].style.color = "blue";
+    trend_information_elements[0].innerHTML = '#' + json_obj_data.name;
+
+    // Trend TOTAL TWEETS
+    trend_information_elements[1].style.color = "blue";
+    if(json_obj_data.total_tweet_volume == "0"){
+        trend_information_elements[1].innerHTML = "No Data";
+    }
+    else{
+        trend_information_elements[1].innerHTML = json_obj_data.total_tweet_volume;
+    }
+
+    // Trend TOP TRENDING
+    if(json_obj_data.is_top_trending){
+        trend_information_elements[2].style.color = "#118a2c";
+        trend_information_elements[2].innerHTML = "True"
+    }
+    else{
+        trend_information_elements[2].style.color = "red";
+        trend_information_elements[2].innerHTML = "False"
+    }
+
+    // Trend ANALYSIS TWEET QUANTITY
+    trend_information_elements[3].style.color = "blue";
+    trend_information_elements[3].innerHTML = json_obj_data.num_positive + json_obj_data.num_neutral +  json_obj_data.num_negative;
+
+    // Trend Positive Percentage
+    trend_information_elements[4].style.color = "#118a2c";
+    trend_information_elements[4].innerHTML = Math.round( json_obj_data.positive_percentage * 100 ) / 100;
+
+    // Trend Neutral Percentage
+    trend_information_elements[5].style.color = "blue";
+    trend_information_elements[5].innerHTML = Math.round( json_obj_data.neutral_percentage * 100 ) / 100;
+
+    // Trend Negative Percentage
+    trend_information_elements[6].style.color = "red";
+    trend_information_elements[6].innerHTML = Math.round( json_obj_data.negative_percentage * 100 ) / 100;
+}
+
 const fetch_trend = async (temp_pathname) => {
     let temp_url = fetch_trend_by_id_url + temp_pathname + '/';
-    console.log(temp_pathname)
     let response = await fetch(temp_url, {
         method: "GET",
     });
     if(response.ok){
         let json_obj = await response.json();
-        console.log(json_obj)
+        if(json_obj.status){
+            hide_trend_information_data_unavailability_tag();
+            fill_trend_information(json_obj.data);
+        }
+        else{
+            display_trend_information_data_unavailability_tag();
+        }
     }
     else{
         console.log("Error occured, reload the page.");
@@ -62,6 +119,7 @@ function create_and_fill_active_trends(json_obj){
         temp_obj.addEventListener('click', event => {
             unactivate_trending_button();
             temp_obj.children[0].classList.add('active');
+            fetch_trend(item.id);
         });
     });
 }
@@ -75,6 +133,9 @@ const get_active_trend = async () => {
         if(json_obj.status && json_obj.data.length){
             hide_active_trend_data_unavailability_tag();
             create_and_fill_active_trends(json_obj);
+        }
+        else{
+            display_active_trend_data_unavailability_tag();
         }
     }
     else{
