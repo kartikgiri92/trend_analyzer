a43e099593609b6b121da4d6263b2e1c53997bd7
kartikgiri92 <kartikgiri92@gmail.com>
2020-02-03 20:57:26 +0530

    a

---
+++ mysite_config.py
@@ -0,0 +1,15 @@
+twitter_api_key = 'nTJ9Cy6xyKJrdnQnM65qEBJdZ'
+
+twitter_api_secret_key = 'H9ZlGWWl4FIlUN2f67pn0Yo0yYIXZCkSbI6HP12zxT1X0s9EIW'
+
+dev_keys = {
+    "secret_key" : "gpt6&c=x^$(g&um*--l%vx-eeq%8+x^vgf_^$=t!1g!h8i++*#",
+    "db_name" : "twitter_proj",
+    "db_user" : "root",
+    "db_pass" : "root1234",
+    "host" : "localhost"
+}
+
+prod_keys = {
+    "secret_key" : "gpt6&c=x^$(g&um*--l%vx-eeq%8+x^vgf_^$=t!1g!h8i++*#",
+}
