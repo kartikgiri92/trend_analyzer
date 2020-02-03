6174dfb1b35fbd08529b5f1428b4cc46562ce087
kartikgiri92 <kartikgiri92@gmail.com>
2020-02-03 20:59:00 +0530

    added aws url

--- config/production.py
+++ config/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = mysite_config.prod_keys['secret_key']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['env-1.b6pimk2whj.ap-south-1.elasticbeanstalk.com']
 
 if('RDS_HOSTNAME' in os.environ):
     DATABASES = {
