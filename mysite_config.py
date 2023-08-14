29fb4d4cece371e55c34466369ff410d2c017fc4
Kartik Giri <kartikgiri92@gmail.com>
2023-08-14 14:53:17 +0530

    Delete Unused Vars (Still visible in Old Commits)

--- mysite_config.py
+++ mysite_config.py
@@ -1,8 +1,8 @@
-django_secret_key = "gpt6&c=x^$(g&um*--l%vx-eeq%8+x^vgf_^$=t!1g!h8i++*#"
-foung_required_key = 'jfhjturicdkjvnfjdksdkdsjkfd854784589fduidfjhfsjh342897'
+django_secret_key = ""
+foung_required_key = ''
 twitter_cred = {
-   'twitter_api_key': 'nTJ9Cy6xyKJrdnQnM65qEBJdZ',
-   'twitter_api_secret_key': 'H9ZlGWWl4FIlUN2f67pn0Yo0yYIXZCkSbI6HP12zxT1X0s9EIW'
+   'twitter_api_key': '',
+   'twitter_api_secret_key': ''
 }
 
 # This is Publicly visible on js.
